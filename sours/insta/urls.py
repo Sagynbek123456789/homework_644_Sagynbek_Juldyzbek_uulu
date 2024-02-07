488d3fe24a9d6ee16f95a8436c37feeeb9878688
Sagynbek <jyldyzbekov-s@mail.ru>
2024-02-07 22:19:53 +0600

    Создал приложение API и настроил его в настройках проекта и настроил URLS в проекте

--- sours/insta/urls.py
+++ sours/insta/urls.py
@@ -19,9 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+urls_api = [
+    path('v1/', include('api_v1.urls'))
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('api/', include(urls_api))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
