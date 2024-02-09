0bebb34cd84a92cabfe156f0b96333cfd70c1d14
Sagynbek <jyldyzbekov-s@mail.ru>
2024-02-10 00:23:16 +0600

    добавил настройки прложения

--- sours/api_v1/urls.py
+++ sours/api_v1/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api_v1.views import PublicationViewSet, LogoutView
+from rest_framework.authtoken.views import obtain_auth_token
 
-# app_name = 'api_v1'
+app_name = 'api_v1'
+
+router = routers.DefaultRouter()
+router.register(r'publications', PublicationViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('login/', obtain_auth_token, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
+
